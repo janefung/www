9dd024cb3683b187fe0fa189a3f6e4ac85ed29b4
Ryan Sobol <contact@ryansobol.com>
2012-08-24 15:32:20 -0700

    Add iWeb theme

--- config.ru
+++ config.ru
@@ -1,3 +1,8 @@
 # encoding: UTF-8
 require "mango"
+
+class Mango::Application
+  set :theme, "iWeb"
+end
+
 run Mango::Application
---
+++ themes/iWeb/public/Its_Nice_to_Meet_You!_files/Its_Nice_to_Meet_You!.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120824
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Its_Nice_to_Meet_You%21_files/FormalShadow_01.png'),IWCreateImage('Its_Nice_to_Meet_You%21_files/FormalShadow_02.png'),IWCreateImage('Its_Nice_to_Meet_You%21_files/FormalShadow_03.png'),IWCreateImage('Its_Nice_to_Meet_You%21_files/FormalShadow_06.png'),IWCreateImage('Its_Nice_to_Meet_You%21_files/FormalShadow_12.png'),IWCreateImage('Its_Nice_to_Meet_You%21_files/FormalShadow_11.png'),IWCreateImage('Its_Nice_to_Meet_You%21_files/FormalShadow_10.png'),IWCreateImage('Its_Nice_to_Meet_You%21_files/FormalShadow_04.png')],null,2,0.700000,1.000000,5.000000,1.000000,4.000000,17.000000,17.000000,17.000000,36.000000,4.000000,837.000000,4.000000,837.000000,null,null,null,0.100000),shadow_0:new IWShadow({blurRadius:6,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.690000}),stroke_1:new IWEmptyStroke(),shadow_1:new IWShadow({blurRadius:6,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.690000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Its_Nice_to_Meet_You!_files/Its_Nice_to_Meet_You!Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
